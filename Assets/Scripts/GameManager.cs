fb7fc389cb658a5405292a6ec4d77d614aba9cb8
Ernest Gomis <gxmis791@gmail.com>
2021-04-12 12:52:57 +0200

    Scene de MainMenu creada + GameManager generado y casi terminado

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public Scene currentScene;
+
+    void Awake()
+    {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        } else {
+            Debug.Log("Warning: multiple " + this + " in scene!");
+        }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene("Room");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
