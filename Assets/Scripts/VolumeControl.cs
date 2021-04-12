884782eab4aa21e377068f976b844b76b368688b
Albert <Lucas>
2021-04-12 19:01:36 +0200

    Diseño de nivel, maravilla

--- Assets/Scripts/VolumeControl.cs
+++ Assets/Scripts/VolumeControl.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 using UnityEngine;
 
-public class VolumeControl : MonoBehaviour
+public class FXVolumeControl : MonoBehaviour
 {
+    [SerializeField] string volumeParameter = "MusicVolume";
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] Slider slider;
+    [SerializeField] float multiplier=30f;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        slider.onValueChanged.AddListener(SliderValueChanged);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void SliderValueChanged(float value){
+        mixer.SetFloat(volumeParameter, value:Mathf.Log10(value)* multiplier);
     }
 }
