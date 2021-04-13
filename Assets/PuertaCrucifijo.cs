33b56e9a1ccdf0c4bf1cbbba2bea6eb2b5928d51
Ernest Gomis <gxmis791@gmail.com>
2021-04-13 23:57:29 +0200

    Puertas se destruyen con crucifijo + el Personaje

---
+++ Assets/PuertaCrucifijo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuertaCrucifijo : MonoBehaviour
+{
+    public BoxCollider2D Door;
+    
+    private SpriteRenderer Sprite;
+    private bool DoorClosed = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sprite = GetComponent<SpriteRenderer>();
+        Sprite.enabled = true;
+        Door.enabled = true;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Crucifijo")
+        {
+            if (DoorClosed)
+            {
+                Sprite.enabled = false;
+                Door.enabled = false;
+                DoorClosed = false;
+                GetComponent<BoxCollider2D>().enabled = false;
+                
+            }
+
+        }
+    }
+}
